585fcf304f2a284a1dfc52e42447f594b06423ab
chanceDira <chancedesire60@gmail.com>
2022-03-16 22:56:51 +0200

    Updates

---
+++ precache-manifest.ed25b4f3b5199854fa7a4fc717864eea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6d113cc2ac2db107d4075b0e6d7c44",
+    "url": "/ether-lottery/index.html"
+  },
+  {
+    "revision": "f6a852f082f1e218168c",
+    "url": "/ether-lottery/static/css/main.b62a1f12.chunk.css"
+  },
+  {
+    "revision": "63128bb6c5ba99e9f640",
+    "url": "/ether-lottery/static/js/2.05ad24c8.chunk.js"
+  },
+  {
+    "revision": "deea066462e3852fb7f610c3fa67aa20",
+    "url": "/ether-lottery/static/js/2.05ad24c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a852f082f1e218168c",
+    "url": "/ether-lottery/static/js/main.1df229e0.chunk.js"
+  },
+  {
+    "revision": "1ad8422dde33307203be",
+    "url": "/ether-lottery/static/js/runtime-main.d43499aa.js"
+  },
+  {
+    "revision": "7ec1e2327b3658b4ab640947dbbb74be",
+    "url": "/ether-lottery/static/media/wallpaper.7ec1e232.png"
+  }
+]);
